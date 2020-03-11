24d58bcf7b65cc2b29becb6259473161152bee4e
Ryan <gal17027@byui.edu>
2020-03-11 17:40:37 -0600

    Added take and upload activities for photo. Also added navigation.

--- app/src/main/java/com/example/profileassistant/PhotoActivity.java
+++ app/src/main/java/com/example/profileassistant/PhotoActivity.java
@@ -3,6 +3,7 @@ package com.example.profileassistant;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,24 @@ public class PhotoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent backIntent = new Intent(v.getContext(), MainActivity.class);
                 startActivity(backIntent);
+            }
+        });
+
+        //Use ph_takePhoto button to open TakePhotoActivity
+        Button takeButton = (Button) findViewById(R.id.ph_takePhoto);
+        takeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v1) {
+                Intent tIntent= new Intent(v1.getContext(), TakePhotoActivity.class);
+                startActivity(tIntent);
+            }
+        });
+
+        //Use ph_uploadPhoto button to open UploadActivity
+        Button uploadButton = (Button) findViewById(R.id.ph_uploadPhoto);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v2) {
+                Intent uIntent = new Intent(v2.getContext(), UploadActivity.class);
+                startActivity(uIntent);
             }
         });
     }
--- app/src/main/java/com/example/profileassistant/UploadActivity.java
+++ app/src/main/java/com/example/profileassistant/UploadActivity.java
@@ -1,6 +1,9 @@
 package com.example.profileassistant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        //Use back_arrow to navigate back to photo page without edits
+        ImageButton backArrow = (ImageButton) findViewById(R.id.back_arrow7);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent backIntent = new Intent(v.getContext(), PhotoActivity.class);
+                startActivity(backIntent);
+            }
+        });
     }
 }
